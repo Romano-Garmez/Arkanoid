e5cfe5182f0cd20e4884d66181519716afa6d7a2
roman garms <romangarms@gmail.com>
2022-09-19 09:26:43 -0700

    project complete

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    //movement speed
+    public float speed = 100.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
     {
-        
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "racket")
+        {
+            float x =
+                hitFactor(transform.position,
+                col.transform.position,
+                col.collider.bounds.size.x);
+
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
     }
 }
